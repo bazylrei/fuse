0e465798495a05ff53a428cb828dc503f3efea75
Bazyl Reinstein <Bazyl.Reinstein@bjss.com>
2016-09-05 20:34:37 +0100

    inital build

---
+++ Fuse/Fuse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fuse
+//
+//  Created by Bazyl Reinstein on 05/09/2016.
+//  Copyright © 2016 BazylRei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
